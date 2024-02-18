a29c41b47318a2373165226f229a40ce05a4200e
Perceptua <perceptua.blue@gmail.com>
2024-02-18 17:50:39 -0500

    add form verification to capture E-Trade verification code

--- snaketrade/app.py
+++ snaketrade/app.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, redirect, render_template
-from auth import Auth
+from flask import Flask, redirect, render_template, request
+from snaketrade.auth import Auth
 
 
 app = Flask(__name__)
+auth = None
 
 
 @app.route('/')
@@ -12,8 +13,18 @@ def home():
 
 @app.route('/authorize/<env>')
 def authorize(env):
-    authorize_url = Auth(env).get_authorize_url()
+    auth = Auth(env)
+    authorize_url = auth.get_authorize_url()
     
     return redirect(authorize_url)
+
+@app.route('/verify', methods=['POST'])
+def verify():
+    verification_code = None
+    
+    if request.method == 'POST':
+        verification_code = request.form['verification_code']
+        
+    return {'verification_code': verification_code}
     
         
