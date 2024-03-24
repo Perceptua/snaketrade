fda093695a19bc10c8c83a2a41888328751236b6
Perceptua <perceptua.blue@gmail.com>
2024-03-24 14:21:53 -0400

    apply PEP8 to app.py

--- snaketrade/app.py
+++ snaketrade/app.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, redirect, render_template, request
 from snaketrade.auth import Auth
 
@@ -11,20 +10,21 @@ auth = Auth('sandbox')
 def home():
     return render_template('home.html')
 
+
 @app.route('/authorize/<env>')
 def authorize(env):
     auth.env = env
     auth.set_auth_components()
-    
+
     return redirect(auth.authorize_url)
+
 
 @app.route('/verify', methods=['POST'])
 def verify():
     verification_code = None
-    
+
     if request.method == 'POST':
         verification_code = request.form['verification_code']
         auth.make_session(verification_code)
-        
+
     return {'verification_code': verification_code}
-        
