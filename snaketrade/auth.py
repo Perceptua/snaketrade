0907078fcb17c49a85d3c50130b097995698f9d6
Perceptua <perceptua.blue@gmail.com>
2024-03-24 14:20:36 -0400

    apply PEP 8 to auth module

--- snaketrade/auth.py
+++ snaketrade/auth.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 from rauth import OAuth1Service
 import os
+
 
 class Auth:
     def __init__(self, env):
@@ -11,55 +11,55 @@ class Auth:
         self.request_token_secret = None
         self.authorize_url = None
         self.session = None
-        
+
         self.env_map = {
             'sandbox': 'https://apisb.etrade.com',
             'prod': 'https://api.etrade.com'
         }
-        
+
     def set_auth_components(self):
         self.base_url = self.get_base_url()
         consumer_key, consumer_secret = self.get_consumer_key()
-        
+
         self.oauth_service = self.get_oauth_service(
-            self.base_url, 
-            consumer_key, 
+            self.base_url,
+            consumer_key,
             consumer_secret
         )
-        
+
         self.request_token, self.request_token_secret = self.get_request_token(
             self.oauth_service
         )
-        
+
         self.authorize_url = self.get_authorize_url(
-            self.oauth_service, 
+            self.oauth_service,
             self.request_token
         )
-        
+
     def make_session(self, verification_code):
         self.session = self.oauth_service.get_auth_session(
             self.request_token,
             self.request_token_secret,
             params=dict(oauth_verifier=verification_code),
         )
-                                
+
     def get_base_url(self):
         base_url = self.env_map[self.env]
-            
+
         return base_url
-    
+
     def get_consumer_key(self):
         env_str = f'ETRADE_{self.env.upper()}'
         consumer_key = os.environ[f'{env_str}_KEY']
         consumer_secret = os.environ[f'{env_str}_SECRET']
-        
+
         return consumer_key, consumer_secret
-    
+
     def get_oauth_service(self, base_url, consumer_key, consumer_secret):
         params = 'key={consumer_key}&token={request_token}'
-        
+
         oauth_service = OAuth1Service(
-            consumer_key=consumer_key, 
+            consumer_key=consumer_key,
             consumer_secret=consumer_secret,
             name='etrade',
             request_token_url=f'{base_url}/oauth/request_token',
@@ -67,21 +67,20 @@ class Auth:
             authorize_url=f'https://us.etrade.com/e/t/etws/authorize?{params}',
             base_url=base_url
         )
-        
+
         return oauth_service
-    
+
     def get_request_token(self, oauth_service):
         request_token, request_token_secret = oauth_service.get_request_token(
             params=dict(oauth_callback='oob', format='json')
         )
-        
+
         return request_token, request_token_secret
-    
+
     def get_authorize_url(self, oauth_service, request_token):
         authorize_url = oauth_service.authorize_url.format(
             consumer_key=oauth_service.consumer_key,
             request_token=request_token
         )
-        
+
         return authorize_url
-    
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from snaketrade.auth import Auth
 from rauth import OAuth1Service, OAuth1Session
 import unittest as ut
@@ -11,95 +10,95 @@ class TestAuth(ut.TestCase):
         auth = Auth(env)
         self.assertIsInstance(auth, Auth)
         self.assertEqual(auth.env, env)
-        
+
         env = 'prod'
         auth = Auth(env)
         self.assertIsInstance(auth, Auth)
         self.assertEqual(auth.env, env)
-        
+
         env = 'invalid'
         auth = Auth(env)
         self.assertIsInstance(auth, Auth)
         self.assertEqual(auth.env, env)
-        
+
     def testGetBaseURL(self):
         env = 'sandbox'
         auth = Auth(env)
         base_url = auth.get_base_url()
         self.assertIn('apisb.etrade.com', base_url)
-        
+
         env = 'prod'
         auth = Auth(env)
         base_url = auth.get_base_url()
         self.assertIn('api.etrade.com', base_url)
-        
+
         env = 'invalid'
         auth = Auth(env)
         self.assertRaises(KeyError, auth.get_base_url)
-        
+
     def testGetConsumerKey(self):
         env = 'sandbox'
         auth = Auth(env)
         consumer_key, consumer_secret = auth.get_consumer_key()
         self.assertIsInstance(consumer_key, str)
         self.assertIsInstance(consumer_secret, str)
-        
+
         env = 'prod'
         auth = Auth(env)
         consumer_key, consumer_secret = auth.get_consumer_key()
         self.assertIsInstance(consumer_key, str)
         self.assertIsInstance(consumer_secret, str)
-        
+
         env = 'invalid'
         auth = Auth(env)
         self.assertRaises(KeyError, auth.get_consumer_key)
-        
+
     def testGetOauthService(self):
         env = 'sandbox'
         auth = Auth(env)
         base_url = auth.get_base_url()
         consumer_key, consumer_secret = auth.get_consumer_key()
-        
+
         oauth_service = auth.get_oauth_service(
             base_url, consumer_key, consumer_secret
         )
-        
+
         self.assertIsInstance(oauth_service, OAuth1Service)
-        
+
     def testGetRequestToken(self):
         env = 'sandbox'
         auth = Auth(env)
         base_url = auth.get_base_url()
         consumer_key, consumer_secret = auth.get_consumer_key()
-        
+
         oauth_service = auth.get_oauth_service(
             base_url, consumer_key, consumer_secret
         )
-        
+
         request_token, request_token_secret = auth.get_request_token(
             oauth_service
         )
-        
+
         self.assertIsInstance(request_token, str)
         self.assertIsInstance(request_token_secret, str)
-        
+
     def testGetAuthorizeURL(self):
         env = 'sandbox'
         auth = Auth(env)
         base_url = auth.get_base_url()
         consumer_key, consumer_secret = auth.get_consumer_key()
-        
+
         oauth_service = auth.get_oauth_service(
             base_url, consumer_key, consumer_secret
         )
-        
+
         request_token, _ = auth.get_request_token(
             oauth_service
         )
-        
+
         authorize_url = auth.get_authorize_url(oauth_service, request_token)
         self.assertIn('https://us.etrade.com/e/t/etws', authorize_url)
-        
+
     def testSetAuthComponents(self):
         env = 'sandbox'
         auth = Auth(env)
@@ -109,7 +108,7 @@ class TestAuth(ut.TestCase):
         self.assertIsInstance(auth.request_token, str)
         self.assertIsInstance(auth.request_token_secret, str)
         self.assertIn('https://us.etrade.com/e/t/etws', auth.authorize_url)
-        
+
     def testMakeSession(self):
         env = 'sandbox'
         auth = Auth(env)
@@ -118,4 +117,3 @@ class TestAuth(ut.TestCase):
         verification_code = input('Enter verification code: ')
         auth.make_session(verification_code)
         self.assertIsInstance(auth.session, OAuth1Session)
-        
