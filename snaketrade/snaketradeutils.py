6dd7146a711c5c2dedb3f33e363f7474a21bb515
Perceptua <perceptua.blue@gmail.com>
2024-03-23 21:15:45 -0400

    parse response JSON, parse dictionaries to dataframes, convert ms since epoch to UTC datetime

--- snaketrade/snaketradeutils.py
+++ snaketrade/snaketradeutils.py
@@ -1,9 +1,84 @@
 """Helper classes for snaketrade."""
-from datetime import datetime as dt
+from datetime import datetime, timedelta, timezone
+import json
+import pandas as pd
 
 
 class SnakeTradeUtils:
     """Utility functions for snaketrade."""
+
+    def parse_response_json(response):
+        """
+        Deserialize response JSON into dictionary.
+
+        Requires a response with status code 200. If status code is not
+        200, ValueError is raised.
+
+        Parameters
+        ----------
+        response : requests.Response
+            An HTTP response object.
+
+        Raises
+        ------
+        ValueError
+            If response status code is not 200, raise ValueError.
+
+        Returns
+        -------
+        data : dict or list
+            Parsed response JSON.
+
+        """
+        data = {}
+
+        if response.status_code == 200:
+            data = json.loads(response.text)
+        else:
+            error_message = f'{response.status_code}: {response.reason}'
+            raise ValueError(error_message)
+
+        return data
+
+    def dict_to_dataframe(data, flatten=True):
+        """
+        Parse dictionary data into a single-row dataframe.
+
+        If the optional flatten parameter is set to true, nested dictionaries
+        will be parsed & resulting dataframes will be concatenated with parent
+        dictionaries along axis 1 (columns). Note that values of type list will
+        not be parsed.
+
+        Parameters
+        ----------
+        data : dict
+            Dictionary to be parsed into dataframe.
+        flatten : bool, optional
+            If true, parse nested dictionaries & concatenate resulting
+            dataframes to the parent dictionary along axis 1 (columns).
+            The default is True.
+
+        Returns
+        -------
+        dataframe : pandas.DataFrame
+            Single-row dataframe of dictionary data.
+
+        """
+        formatted = {}
+        subframes = []
+
+        for key, value in data.items():
+            if flatten and type(value) == dict:
+                subframes += [
+                    SnakeTradeUtils.dict_to_dataframe(value, flatten)
+                ]
+            else:
+                formatted[key] = [value]
+
+        subframes += [pd.DataFrame(data=formatted)]
+        dataframe = pd.concat(subframes, axis=1)
+
+        return dataframe
 
     def check_date_format(date_string, format_string, raise_error=True):
         """
@@ -34,10 +109,35 @@ class SnakeTradeUtils:
         matches_format = False
 
         try:
-            dt.strptime(date_string, format_string)
+            datetime.strptime(date_string, format_string)
             matches_format = True
         except Exception as error:
             if raise_error:
                 raise error
 
         return matches_format
+
+    def utc_from_milliseconds(milliseconds):
+        """
+        Convert milliseconds since epoch to datetime object with UTC timezone.
+
+        Parameters
+        ----------
+        milliseconds : str or int
+            Milliseconds since epoch (1970-01-01 00:00:000.000).
+
+        Returns
+        -------
+        utc_timestamp : datetime.datetime
+            Datetime object representing seconds from epoch in UTC timezone.
+
+        """
+        milliseconds = int(milliseconds)
+
+        utc_timestamp = datetime(
+            year=1970, month=1, day=1, tzinfo=timezone.utc
+        ) + timedelta(
+            milliseconds=milliseconds
+        )
+
+        return utc_timestamp
--- tests/test_snaketradeutils.py
+++ tests/test_snaketradeutils.py
@@ -1,8 +1,51 @@
+from datetime import datetime
 from snaketrade.snaketradeutils import SnakeTradeUtils as stu
+import json
+import requests
 import unittest as ut
 
 
 class TestSnakeTradeUtils(ut.TestCase):
+    @classmethod
+    def getValueTypes(cls, dataframe):
+        first_row = dataframe.loc[0]
+        value_types = [type(i) for i in first_row]
+
+        return value_types
+
+    def testParseResponseJSON(self):
+        valid_url = 'https://api.github.com/users/perceptua/repos'
+        invalid_url = 'https://api.github.com/nonsense'
+
+        response = requests.get(valid_url)
+        data = stu.parse_response_json(response)
+        self.assertTrue(type(data) == dict or type(data) == list)
+
+        response = requests.get(invalid_url)
+        self.assertRaises(ValueError, stu.parse_response_json, response)
+
+    def testDictToDataframe(self):
+        sample_json = 'tests/data/sample_balance_response_prod.json'
+        sample_data = json.load(open(sample_json))['BalanceResponse']
+        dataframe = stu.dict_to_dataframe(sample_data, flatten=True)
+        expected_shape = (1, 32)
+        value_types = self.getValueTypes(dataframe)
+        self.assertEqual(dataframe.shape, expected_shape)
+        self.assertNotIn(dict, value_types)
+
+        dataframe = stu.dict_to_dataframe(sample_data, flatten=False)
+        expected_shape = (1, 9)
+        value_types = self.getValueTypes(dataframe)
+        self.assertEqual(dataframe.shape, expected_shape)
+        self.assertIn(dict, value_types)
+
+    def testDictToDataframeList(self):
+        sample_json = 'tests/data/sample_transaction_list_response_prod.json'
+        sample_data = json.load(open(sample_json))['TransactionListResponse']
+        dataframe = stu.dict_to_dataframe(sample_data, flatten=True)
+        value_types = self.getValueTypes(dataframe)
+        self.assertIn(list, value_types)
+
     def testCheckDateFormat(self):
         date_str = '02192024'
         fmt_str = '%m%d%Y'
@@ -16,6 +59,14 @@ class TestSnakeTradeUtils(ut.TestCase):
         self.assertRaises(
             Exception, stu.check_date_format, date_str, fmt_str, True
         )
+
+    def testUTCFromMilliseconds(self):
+        date_str = '1711027936000'
+        utc_timestamp = stu.utc_from_milliseconds(date_str)
+        self.assertIsInstance(utc_timestamp, datetime)
+
+        date_str = '2024-03-21 15:54:00.000'
+        self.assertRaises(ValueError, stu.utc_from_milliseconds, date_str)
 
 
 if __name__ == '__main__':
